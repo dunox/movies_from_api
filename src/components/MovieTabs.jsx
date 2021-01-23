8dbaf0255e26bcc8aa5644a8c8a3c7d406e16524
Andriy <34597632+dunox@users.noreply.github.com>
2021-01-23 08:55:56 +0200

    refactor add sort by tabs and refactor

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -2,40 +2,37 @@ import React from "react";
 
 const MovieTabs = (props) => {
   const { sortByMoviesList, updateSortBy } = props;
+
+  const handleClick = (value) => () => {
+    updateSortBy(value);
+  };
+
+  const getClassByValue = (value) => {
+    return `nav-link ${sortByMoviesList === value ? "active" : ""}`;
+  };
+
   return (
     <ul className="tabs nav nav-pills">
       <li className="nav-item">
         <div
-          className={`nav-link ${
-            sortByMoviesList === "popularity.desc" ? "active" : ""
-          }`}
-          onClick={() => {
-            updateSortBy("popularity.desc");
-          }}
+          className={getClassByValue("popularity.desc")}
+          onClick={handleClick("popularity.desc")}
         >
           Popularity
         </div>
       </li>
       <li>
         <div
-          className={`nav-link ${
-            sortByMoviesList === "revenue.desc" ? "active" : ""
-          }`}
-          onClick={() => {
-            updateSortBy("revenue.desc");
-          }}
+          className={getClassByValue("revenue.desc")}
+          onClick={handleClick("revenue.desc")}
         >
           Revenue
         </div>
       </li>
       <li>
         <div
-          className={`nav-link ${
-            sortByMoviesList === "primary_release_date.desc" ? "active" : ""
-          }`}
-          onClick={() => {
-            updateSortBy("primary_release_date.desc");
-          }}
+          className={getClassByValue("primary_release_date.desc")}
+          onClick={handleClick("primary_release_date.desc")}
         >
           Release Date
         </div>
